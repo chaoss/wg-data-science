039ae4d4a2bf01db340c04549fbc9a0cb2b25c20
Dawn M. Foster <dawn@dawnfoster.com>
2024-05-15 10:03:57 +0100

    Created a dataset directory along with the first draft of a script that can used to generate a dataset for license change details given a list of org, repo, license files
    Signed-off-by: Dawn M. Foster <dawn@dawnfoster.com>

---
+++ dataset/license-changes/generate-license-data.py
@@ -0,0 +1,134 @@
+#SPDX-License-Identifier: MIT
+
+# Note: License file must be in the same org/repo
+
+def setup_validate():
+    import argparse
+    import os
+    import csv
+    import sys
+
+    # Gather options from command line arguments and store them in variables
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-c", "--configfile", required=False, dest="input_data_file", default="inputdata.csv", help="The full path to a csv file containing on each line a gh_org_name,gh_repo,name_of_license_file (defaults to inputdata.csv)")
+    parser.add_argument("-t", "--tokenvar", required=False, dest="token_var", default="GITHUB_AUTH_TOKEN", help="The name of the environmental variable where your GitHub personal access token can be found (defaults to GITHUB_AUTH_TOKEN)")
+
+    args = parser.parse_args()
+    input_data_file = args.input_data_file
+    token_var = args.token_var
+
+    # Read GitHub personal access token from the GITHUB_AUTH_TOKEN environment variable
+
+    try:
+        gh_key = os.environ[token_var]
+
+    except:
+        print("You must have an environment variable with a GitHub personal access token to run this script")
+        print("For Linux / MacOS: export GITHUB_AUTH_TOKEN=<your access token>")
+        print("For Windows: set GITHUB_AUTH_TOKEN=<your access token>")
+        print("Exiting ...")
+        sys.exit(1)
+
+    # Read input file with some minimal data validation and store data in a list
+
+    try:
+        input_data = []
+        
+        with open(input_data_file, 'r') as f:
+            data = csv.reader(f)
+            for row in data:
+                # Skip blank lines
+                if len(row) != 0:
+                    input_data.append(row)
+
+                    # Make sure the csv file has 3 values per row before continuing
+                    if len(row) != 3:
+                        print("Data errors detected in row containing:", row)
+                        print("Each line in the csv must contain 3 values.")
+                        sys.exit(1)
+
+        # Check for empty file and exit
+        if len(input_data) == 0:
+            print(input_data_file, "appears to be empty.")
+            sys.exit(1)
+
+        print("Reading input data from", input_data_file)
+
+        return input_data, gh_key
+
+    except:
+        print("Can't read file", input_data_file, "Exiting ...")
+        sys.exit(1)
+    
+    
+
+def make_query():
+    return """query licenseData($org: String!, $repo: String!, $lic_file: String!){
+        repository(owner: $org, name: $repo){
+            defaultBranchRef {
+                name
+                target {
+                    ... on Commit {
+                    history(path: $lic_file, first: 100) {
+                        nodes {
+                        committedDate
+                        url
+                        additions
+                        deletions
+                        message
+                        }
+                    }
+                    }
+                }
+                }
+            url
+            nameWithOwner
+        }
+    }
+"""
+
+def get_license_data():
+
+    import requests
+    import json
+    import sys
+
+    input_data, api_token = setup_validate()
+    #print(input_data, api_token)
+
+    url = 'https://api.github.com/graphql'
+    headers = {'Authorization': 'token %s' % api_token}
+
+    json_list = []
+
+    for row in input_data:
+        org = row[0]
+        repo = row[1]
+        lic_file = row[2]
+
+        try:
+            query = make_query()
+
+            variables = {"org": org, "repo": repo, "lic_file": lic_file}
+            #variables = {"org": org, "repo": repo}
+            r = requests.post(url=url, json={'query': query, 'variables': variables}, headers=headers)
+            json_data = json.loads(r.text)['data']
+            json_list.append(json_data)
+        
+        except:
+            print("Unknown error: Could not get data from GitHub")
+            sys.exit(1)
+    
+    return json_list
+
+import json
+
+json_list = get_license_data()
+print(json_list)
+
+json_obj = json.dumps(json_list, indent=4)
+
+with open('output.json', 'w') as output:
+    output.write(json_obj)
